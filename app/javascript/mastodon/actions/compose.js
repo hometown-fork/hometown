180ee3f7752aadee3acf59ec27767cf5f52f12a6
Darius Kazemi <darius.kazemi@gmail.com>
2025-12-01 08:41:45 -0500

    Getting rid of string/boolean confusion
    The issue is in the dropdown_selector component, which stores data as a
    `data-index` element on a dropdown. This means all values are cast to
    strings in anything using that dropdown, which means we need to
    unstringify on the `handleChange` function that (up in the component)
    calls the `getAttribue` function. Anyway this means we can remove
    terrible hacks in multiple places.

--- app/javascript/mastodon/actions/compose.js
+++ app/javascript/mastodon/actions/compose.js
@@ -228,7 +228,7 @@ export function submitCompose() {
         spoiler_text: getState().getIn(['compose', 'spoiler']) ? getState().getIn(['compose', 'spoiler_text'], '') : '',
         visibility: getState().getIn(['compose', 'privacy']),
         poll: getState().getIn(['compose', 'poll'], null),
-        local_only: getState().getIn(['compose', 'federation']) === 'false',
+        local_only: !getState().getIn(['compose', 'federation']),
         language: getState().getIn(['compose', 'language']),
       },
       headers: {
