a5780713df2b80ac9c15db342064b79d11336b92
Eugen Rochko <eugen@zeonfederated.com>
2026-07-22 10:39:29 +0100

    Add support for `Link` objects in `attachment` (#36104)
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- app/serializers/activitypub/note_serializer.rb
+++ app/serializers/activitypub/note_serializer.rb
@@ -137,7 +137,7 @@ class ActivityPub::NoteSerializer < ActivityPub::Serializer
   delegate :local_only, to: :object
 
   def virtual_attachments
-    object.ordered_media_attachments
+    object.ordered_media_attachments + [object.preview_card].compact
   end
 
   def virtual_tags
@@ -248,6 +248,18 @@ class ActivityPub::NoteSerializer < ActivityPub::Serializer
         automaticApproval: approved_uris,
       },
     }
+  end
+
+  class PreviewCardSerializer < ActivityPub::Serializer
+    attributes :type, :href
+
+    def type
+      'Link'
+    end
+
+    def href
+      object.original_url.presence || object.url
+    end
   end
 
   class MediaAttachmentSerializer < ActivityPub::Serializer
