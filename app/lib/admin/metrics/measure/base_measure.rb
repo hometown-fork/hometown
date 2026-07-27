624bd5bae0a1602d83396db24c56eb39479a714b
Claire <claire.github-309c@sitedethib.com>
2026-07-27 16:02:02 +0200

    Merge commit from fork
    * Fix GHSA-7jvv-fhmg-wpfw
    
    * Fix GHSA-hx34-2pfw-2qfj
    
    * Fix GHSA-vwhj-3g83-v276
    
    * Bump version to v4.6.4

--- app/lib/admin/metrics/measure/base_measure.rb
+++ app/lib/admin/metrics/measure/base_measure.rb
@@ -12,17 +12,12 @@ class Admin::Metrics::Measure::BaseMeasure
   alias loaded? loaded
 
   def initialize(start_at, end_at, params)
-    @start_at = start_at&.to_datetime
-    @end_at   = end_at&.to_datetime
+    @start_at = start_at.to_datetime
+    @end_at   = end_at.to_datetime
     @params   = params
     @loaded   = false
 
-    if @start_at.present? && @end_at.present?
-      @start_at = [@start_at, @end_at - 2.years].max
-    else
-      @start_at = nil
-      @end_at = nil
-    end
+    @start_at = [@start_at, @end_at - 2.years].max
   end
 
   def cache_key
