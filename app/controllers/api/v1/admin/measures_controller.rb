1f735b229c3eedcdb91f895cf207314bbf6ee1c3
Claire <claire.github-309c@sitedethib.com>
2026-08-11 11:10:52 +0200

    Fix domain block impact queries being rejected (#40122)

--- app/controllers/api/v1/admin/measures_controller.rb
+++ app/controllers/api/v1/admin/measures_controller.rb
@@ -19,8 +19,8 @@ class Api::V1::Admin::MeasuresController < Api::BaseController
   def set_measures
     @measures = Admin::Metrics::Measure.retrieve(
       params.require(:keys),
-      params.require(:start_at),
-      params.require(:end_at),
+      params[:start_at],
+      params[:end_at],
       params
     )
   end
