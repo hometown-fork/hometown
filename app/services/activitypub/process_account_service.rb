34d8823c42a7b684ab257bb7cb27f454d3248c78
Claire <claire.github-309c@sitedethib.com>
2026-08-18 13:21:57 +0000

    Fix error when processing remote actors with `null` public keys (#40194)

--- app/services/activitypub/process_account_service.rb
+++ app/services/activitypub/process_account_service.rb
@@ -435,7 +435,7 @@ class ActivityPub::ProcessAccountService < BaseService
       next unless value['type'] == 'Multikey' && value['controller'] == @account.uri
 
       key_type, key = key_from_multikey(value['publicKeyMultibase'])
-      next if key_type.nil?
+      next if key_type.nil? || key.nil?
 
       { type: key_type, public_key: key, uri: key_id }
     end
